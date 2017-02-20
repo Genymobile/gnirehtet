f41fb2e7a36ec0003d40bd6d77b40faaddd8539f
Romain Vimont <rvimont@genymobile.com>
2017-02-20 11:28:15 +0100

    DatagramBuffer

--- relay/src/main/java/com/genymobile/relay/DatagramBuffer.java
+++ relay/src/main/java/com/genymobile/relay/DatagramBuffer.java
@@ -1,8 +1,99 @@
 package com.genymobile.relay;
 
-/**
- * Created by rom on 14/02/17.
- */
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.WritableByteChannel;
 
+/**
+ * <pre>
+ *     circularBufferLength
+ * |<------------------------->| extra space for storing the last datagram in one block
+ * +---------------------------+------+
+ * |                           |      |
+ * |[B4]     [  B1  ][ B2 ][  B3  ]   |
+ * +---------------------------+------+
+ *     ^     ^
+ *  head     tail
+ * </pre>
+ */
 public class DatagramBuffer {
+
+    public static final String TAG = DatagramBuffer.class.getName();
+
+    // every datagram is stored along with a header storing its length, on 16 bits
+    private static final int HEADER_LENGTH = 2;
+    private static final int MAX_DATAGRAM_LENGTH = 1 << 16;
+    private static final int MAX_BLOCK_LENGTH = HEADER_LENGTH + MAX_DATAGRAM_LENGTH;
+
+    private final byte[] data;
+    private final ByteBuffer wrapper;
+    private int head;
+    private int tail;
+    private final int circularBufferLength;
+
+    public DatagramBuffer(int capacity) {
+        data = new byte[capacity + MAX_BLOCK_LENGTH];
+        wrapper = ByteBuffer.wrap(data);
+        circularBufferLength = capacity + 1;
+    }
+
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
+    public boolean hasEnoughSpaceFor(int datagramLength) {
+        if (head >= tail) {
+            // there is at leat the extra space for storing 1 packet
+            return true;
+        }
+        int remaining = tail - head - 1;
+        return HEADER_LENGTH + datagramLength < remaining;
+    }
+
+    public int capacity() {
+        return circularBufferLength - 1;
+    }
+
+    public int writeTo(WritableByteChannel channel) throws IOException {
+        int length = readLength();
+        wrapper.limit(tail + length).position(tail);
+        tail += length;
+        if (tail >= circularBufferLength) {
+            tail = 0;
+        }
+        int w = channel.write(wrapper);
+        if (w != length) {
+            Log.e(TAG, "Cannot write the whole datagram to the channel (only " + w + "/" + length + ")");
+        }
+        return w;
+    }
+
+    public boolean readFrom(ByteBuffer buffer) {
+        int length = buffer.remaining();
+        if (length > MAX_DATAGRAM_LENGTH) {
+            throw new IllegalArgumentException("Datagram length (" + buffer.remaining() + ") may not be greater than " + MAX_DATAGRAM_LENGTH + " bytes");
+        }
+        if (!hasEnoughSpaceFor(length)) {
+            return false;
+        }
+        writeLength(length);
+        buffer.get(data, head, length);
+        head += length;
+        if (head >= circularBufferLength) {
+            head = 0;
+        }
+        return true;
+    }
+
+    private void writeLength(int length) {
+        assert (length & ~0xffff) == 0 : "Length must be stored on 16 bits";
+        data[head++] = (byte) ((length >> 8) & 0xff);
+        data[head++] = (byte) (length & 0xff);
+    }
+
+    private int readLength() {
+        int length = ((data[tail] & 0xff) << 8) | (data[tail + 1] & 0xff);
+        tail += 2;
+        return length;
+    }
 }
---
+++ relay/src/test/java/com/genymobile/relay/DatagramBufferTest.java
@@ -0,0 +1,95 @@
+package com.genymobile.relay;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
+
+public class DatagramBufferTest {
+
+    private ByteBuffer createDatagram(int size) {
+        byte[] data = new byte[size];
+        for (int i = 0; i < size; ++i) {
+            data[i] = (byte) i;
+        }
+        return ByteBuffer.wrap(data);
+    }
+
+    @Test
+    public void testSimple() throws IOException {
+        ByteBuffer datagram = createDatagram(5);
+
+        DatagramBuffer datagramBuffer = new DatagramBuffer(9);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        WritableByteChannel channel = Channels.newChannel(bos);
+
+        datagramBuffer.readFrom(datagram);
+        datagramBuffer.writeTo(channel);
+
+        byte[] result = bos.toByteArray();
+        Assert.assertArrayEquals(datagram.array(), result);
+    }
+
+    @Test
+    public void testDatagramBoundaries() throws IOException {
+        DatagramBuffer datagramBuffer = new DatagramBuffer(32);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        WritableByteChannel channel = Channels.newChannel(bos);
+
+        ByteBuffer datagram5 = createDatagram(5);
+        ByteBuffer datagram3 = createDatagram(3);
+        ByteBuffer datagram4 = createDatagram(4);
+
+        datagramBuffer.readFrom(datagram5);
+        datagramBuffer.readFrom(datagram3);
+        datagramBuffer.readFrom(datagram4);
+
+        datagramBuffer.writeTo(channel);
+        byte[] result = bos.toByteArray();
+        Assert.assertArrayEquals(datagram5.array(), result);
+
+        bos.reset();
+
+        datagramBuffer.writeTo(channel);
+        result = bos.toByteArray();
+        Assert.assertArrayEquals(datagram3.array(), result);
+
+        bos.reset();
+
+        datagramBuffer.writeTo(channel);
+        result = bos.toByteArray();
+        Assert.assertArrayEquals(datagram4.array(), result);
+    }
+
+    @Test
+    public void testCircular() throws IOException {
+        ByteBuffer datagram5 = createDatagram(5);
+        ByteBuffer datagram3 = createDatagram(3);
+
+        DatagramBuffer datagramBuffer = new DatagramBuffer(14);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        WritableByteChannel channel = Channels.newChannel(bos);
+
+        // write and consume 10 bytes
+        datagramBuffer.readFrom(createDatagram(10));
+        datagramBuffer.writeTo(Channels.newChannel(new ByteArrayOutputStream())); // forget
+
+        datagramBuffer.readFrom(datagram5);
+        datagramBuffer.readFrom(datagram3);
+
+        // DatagramBuffer is expected to store the whole datagram (even if it exceeds its "capacity")
+        datagramBuffer.writeTo(channel);
+        byte[] result = bos.toByteArray();
+        Assert.assertArrayEquals(datagram5.array(), result);
+
+        bos.reset();
+
+        datagramBuffer.writeTo(channel);
+        result = bos.toByteArray();
+        Assert.assertArrayEquals(datagram3.array(), result);
+    }
+}
