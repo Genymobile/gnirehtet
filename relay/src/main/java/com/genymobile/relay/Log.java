c7a97e06527318bcbec9200223c8ce9e3f4d75bf
Romain Vimont <rvimont@genymobile.com>
2017-02-23 17:59:26 +0100

    Add date/time to logs
    Print date and time to logs.

--- relay/src/main/java/com/genymobile/relay/Log.java
+++ relay/src/main/java/com/genymobile/relay/Log.java
@@ -1,20 +1,32 @@
 package com.genymobile.relay;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Log {
+
+    private static final DateFormat FORMAT = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.S");
+    private static final Date date = new Date();
 
     private Log() {
     }
 
+    private static String getDate() {
+        date.setTime(System.currentTimeMillis());
+        return FORMAT.format(date);
+    }
+
     public static void d(String tag, String message) {
-        System.out.println("[" + tag + "] " + message);
+        System.out.println(getDate() + " [" + tag + "] " + message);
     }
 
     public static void e(String tag, String message, Throwable e) {
-        System.err.println("[" + tag + "] " + message);
+        System.err.println(getDate() + " [" + tag + "] " + message);
         e.printStackTrace();
     }
 
     public static void e(String tag, String message) {
-        System.err.println("[" + tag + "] " + message);
+        System.err.println(getDate() + " [" + tag + "] " + message);
     }
 }
