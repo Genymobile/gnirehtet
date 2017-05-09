8c1bbdda95aba223315b370a62bd6522251a79df
Romain Vimont <rvimont@genymobile.com>
2017-05-09 18:19:08 +0200

    HandlerTokenManager

--- rustrelay/src/relay/eventhandler.rs
+++ rustrelay/src/relay/eventhandler.rs
@@ -1,20 +1,46 @@
 use mio::*;
+use std::io;
+use std::collections::HashMap;
 
-struct LambdaPoll {
+struct HandlerTokenManager {
+    token_provider: Box<Iterator<Item=Token>>,
+    handlers: HashMap<Token, Box<EventHandler>>,
+}
+
+impl HandlerTokenManager {
+    fn new() -> HandlerTokenManager {
+        HandlerTokenManager {
+            token_provider: Box::new((0..).map(|x| Token(x))),
+            handlers: HashMap::new(),
+        }
+    }
+    fn create(handler: &EventHandler) -> Token {
+        Token(0)
+    }
+}
+
+struct CallbackPoll {
     poll: Poll,
     events: Events,
     token_provider: Box<Iterator<Item=Token>>,
+    handlers: HashMap<Token, Box<EventHandler>>
 }
 
-impl LambdaPoll {
-    fn new() -> LambdaPoll {
-        LambdaPoll {
-            poll: Poll::new().unwrap(),
+impl CallbackPoll {
+    fn new() -> io::Result<CallbackPoll> {
+        Ok(CallbackPoll {
+            poll: try!(Poll::new()),
             events: Events::with_capacity(1024),
             token_provider: Box::new((0..).map(|x| Token(x))),
-        }
+            handlers: HashMap::new(),
+        })
     }
 
+    fn register<E>(&self, handle: &E, handler: &EventHandler, interest: Ready, opts: PollOpt) -> io::Result<()>
+            where E: Evented + ?Sized {
+        //let token = self.token_provider.next();
+        self.poll.register(handle, Token(0), interest, opts)
+    }
 }
 
 pub trait EventHandler {
