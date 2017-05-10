101ae1e9c91e92e6ceeea09238567d628d24ec0d
Romain Vimont <rvimont@genymobile.com>
2017-05-10 17:56:12 +0200

    Extract the poll-loop into a separate method

--- rustrelay/src/relay/relay.rs
+++ rustrelay/src/relay/relay.rs
@@ -1,6 +1,7 @@
 use mio::*;
 use mio::tcp::TcpListener;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::io;
 
 use super::eventhandler::*;
 
@@ -19,21 +20,24 @@ impl Relay {
         println!("Starting on port {}", self.port);
 
         let mut selector = Selector::new().unwrap();
+        let _server = self.start_socket(&mut selector).expect("Cannot start server");
+        self.poll_loop(&mut selector);
+    }
 
+    fn start_socket(&self, selector: &mut Selector) -> io::Result<TcpListener> {
         let localhost = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
         let addr = SocketAddr::new(localhost, self.port);
-
-        // Setup the server socket
-        let server = TcpListener::bind(&addr).expect("Cannot bind socket");
-
-        // Start listening for incoming connections
+        let server = TcpListener::bind(&addr)?;
         let handler = Box::new(|ready| {
             println!("Ready! {:?}", ready);
         });
-        selector.register(&server, handler, Ready::readable(), PollOpt::edge()).unwrap();
+        selector.register(&server, handler, Ready::readable(), PollOpt::edge())?;
+        Ok(server)
+    }
 
+    fn poll_loop(&self, selector: &mut Selector) {
         loop {
-            selector.select(None).unwrap();
+            selector.select(None).expect("Cannot poll");
 
             for event in &selector.events {
                 println!("event={:?}", event);
