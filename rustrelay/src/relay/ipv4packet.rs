eb79e4ebeee0c1371a28bb5165aa6830606d8af3
Romain Vimont <rvimont@genymobile.com>
2017-05-23 10:53:33 +0200

    Rename header to ipv4_header

--- rustrelay/src/relay/ipv4packet.rs
+++ rustrelay/src/relay/ipv4packet.rs
@@ -2,15 +2,15 @@ use super::ipv4header::IPv4Header;
 
 pub struct IPv4Packet<'a> {
     raw: &'a mut [u8],
-    header: IPv4Header,
+    ipv4_header: IPv4Header,
 }
 
 impl<'a> IPv4Packet<'a> {
     fn new(raw: &'a mut [u8]) -> IPv4Packet<'a> {
-        let header = IPv4Header::parse(raw);
+        let ipv4_header = IPv4Header::parse(raw);
         IPv4Packet {
             raw: raw,
-            header: header,
+            ipv4_header: ipv4_header,
         }
     }
 }
