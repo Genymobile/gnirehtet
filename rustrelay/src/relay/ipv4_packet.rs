7eb9990065bb28bb743b8daa95c8467eee6d78f1
Romain Vimont <rvimont@genymobile.com>
2017-05-23 13:02:25 +0200

    Implement TCP/UDP headers

--- rustrelay/src/relay/ipv4_packet.rs
+++ rustrelay/src/relay/ipv4_packet.rs
@@ -1,16 +1,29 @@
-use super::ipv4_header::IPv4Header;
+use super::ipv4_header::{IPv4Header, Protocol};
+use super::tcp_header::TCPHeader;
+use super::transport_header::TransportHeader;
+use super::udp_header::UDPHeader;
 
 pub struct IPv4Packet<'a> {
     raw: &'a mut [u8],
     ipv4_header: IPv4Header,
+    transport_header: Option<TransportHeader>,
 }
 
 impl<'a> IPv4Packet<'a> {
     fn new(raw: &'a mut [u8]) -> IPv4Packet<'a> {
         let ipv4_header = IPv4Header::parse(raw);
+        let transport_header = {
+            let payload = &raw[ipv4_header.total_length as usize..];
+            match ipv4_header.protocol {
+                Protocol::UDP => Some(TransportHeader::UDP(UDPHeader::parse(payload))),
+                Protocol::TCP => Some(TransportHeader::TCP(TCPHeader::parse(payload))),
+                _ => None
+            }
+        };
         IPv4Packet {
             raw: raw,
             ipv4_header: ipv4_header,
+            transport_header: transport_header,
         }
     }
 }
--- rustrelay/src/relay/udp_header.rs
+++ rustrelay/src/relay/udp_header.rs
@@ -1,4 +1,15 @@
+use byteorder::{BigEndian, ByteOrder};
+
 pub struct UDPHeader {
     source_port: u16,
     destination_port: u16,
+}
+
+impl UDPHeader {
+    pub fn parse(raw: &[u8]) -> UDPHeader {
+        UDPHeader {
+            source_port: BigEndian::read_u16(&raw[0..2]),
+            destination_port: BigEndian::read_u16(&raw[2..4]),
+        }
+    }
 }
