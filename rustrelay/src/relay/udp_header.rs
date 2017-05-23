5b8bad52580e8dfb0093df80518037ae43a98b4c
Romain Vimont <rvimont@genymobile.com>
2017-05-23 15:12:04 +0200

    Implement setters for TCP/UDP headers

--- rustrelay/src/relay/udp_header.rs
+++ rustrelay/src/relay/udp_header.rs
@@ -1,5 +1,7 @@
 use byteorder::{BigEndian, ByteOrder};
 
+const UDP_HEADER_LENGTH: u16 = 8;
+
 pub struct UDPHeader {
     source_port: u16,
     destination_port: u16,
@@ -11,5 +13,25 @@ impl UDPHeader {
             source_port: BigEndian::read_u16(&raw[0..2]),
             destination_port: BigEndian::read_u16(&raw[2..4]),
         }
+    }
+
+    pub fn set_source_port(&mut self, raw: &mut [u8], source_port: u16) {
+        self.source_port = source_port;
+        BigEndian::write_u16(&mut raw[0..2], source_port);
+    }
+
+    pub fn set_destination_port(&mut self, raw: &mut [u8], destination_port: u16) {
+        self.destination_port = destination_port;
+        BigEndian::write_u16(&mut raw[2..4], destination_port);
+    }
+
+    pub fn set_payload_length(&mut self, raw: &mut [u8], payload_length: u16) {
+        let total_length = UDP_HEADER_LENGTH + payload_length;
+        BigEndian::write_u16(&mut raw[4..6], total_length);
+    }
+
+    pub fn compute_checksum(&mut self, raw: &mut [u8]) {
+        // disable checksum validation
+        BigEndian::write_u16(&mut raw[6..8], 0);
     }
 }
