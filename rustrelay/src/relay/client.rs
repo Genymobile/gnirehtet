f3e2284bd384990e8eb01954ab5e5fae71ee7dbb
Romain Vimont <rvimont@genymobile.com>
2017-05-30 18:08:19 +0200

    Client event handling skeleton

--- rustrelay/src/relay/client.rs
+++ rustrelay/src/relay/client.rs
@@ -11,6 +11,7 @@ pub struct Client {
     id: u32,
     stream: TcpStream,
     client_to_network: IPv4PacketBuffer,
+    dead: bool,
 }
 
 impl Client {
@@ -19,15 +20,38 @@ impl Client {
             id: id,
             stream: stream,
             client_to_network: IPv4PacketBuffer::new(),
+            dead: true,
         }));
         // on start, we are interested only in writing (we must first send the client id)
         selector.register(&rc.borrow().stream, rc.clone(), Ready::writable(), PollOpt::level())?;
         Ok(rc)
     }
+
+    fn process_send(&mut self) {
+
+    }
+
+    fn process_receive(&mut self) {
+
+    }
+
+    fn update_interests(&mut self, selector: &mut Selector) {
+
+    }
 }
 
 impl EventHandler for Client {
     fn on_ready(&mut self, selector: &mut Selector, event: Event) {
-
+        assert!(!self.dead);
+        let ready = event.readiness();
+        if ready.is_writable() {
+            self.process_send();
+        }
+        if !self.dead && ready.is_readable() {
+            self.process_receive();
+        }
+        if !self.dead {
+            self.update_interests(selector);
+        }
     }
 }
