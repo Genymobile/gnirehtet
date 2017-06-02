358699a7b0601de9d8ec934327f5292baeb76597
Romain Vimont <rvimont@genymobile.com>
2017-06-02 12:14:20 +0200

    Implement StreamBuffer.read_from()

--- rustrelay/src/relay/stream_buffer.rs
+++ rustrelay/src/relay/stream_buffer.rs
@@ -16,6 +16,18 @@ impl StreamBuffer {
         }
     }
 
+    pub fn size(&self) -> usize {
+        if self.head < self.tail {
+            self.head + self.buf.len() - self.tail
+        } else {
+            self.head - self.tail
+        }
+    }
+
+    pub fn remaining(&self) -> usize {
+        self.buf.len() - self.size()
+    }
+
     pub fn write_to<W: io::Write>(&mut self, destination: &mut W) -> io::Result<usize> {
         if self.head > self.tail {
             let source_slice = &self.buf[self.tail..self.head];
@@ -28,12 +40,30 @@ impl StreamBuffer {
             self.tail = (self.tail + w) % self.buf.len();
             Ok(w)
         } else {
+            // else head == tail, which means empty buffer, nothing to do
             Ok(0)
         }
     }
 
-    pub fn read_from(&mut self, source: &[u8]) -> io::Result<()> {
-        // TODO
-        Ok(())
+    pub fn read_from(&mut self, source: &[u8]) {
+        assert!(self.remaining() >= source.len(), "StreamBuffer must have enough space, check remaining() space before calling read_from()");
+        let source_len = source.len();
+        let buf_len = self.buf.len();
+        if source_len <= buf_len - self.head {
+            let target_slice = &mut self.buf[self.head..self.head + source_len];
+            target_slice.copy_from_slice(source);
+        } else {
+            {
+                // fill until the right-end of the buffer
+                let target_slice = &mut self.buf[self.head..];
+                let source_slice = &source[..source_len - self.head];
+                target_slice.copy_from_slice(source_slice);
+            }
+            // fill the remaining from the left-end of the buffer
+            let target_slice = &mut self.buf[..self.head + source_len - buf_len];
+            let source_slice = &source[source_len - self.head..];
+            target_slice.copy_from_slice(source_slice);
+        }
+        self.head = (self.head + source_len) % buf_len;
     }
 }
