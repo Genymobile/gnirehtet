8e49ca9b392b4b9c9c73d559c3aa9b400a082ed2
Romain Vimont <rvimont@genymobile.com>
2017-06-08 12:02:27 +0200

    Improve Router skeleton

--- rustrelay/src/relay/router.rs
+++ rustrelay/src/relay/router.rs
@@ -1,5 +1,6 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+use log::LogLevel;
 
 use super::client::Client;
 use super::ipv4_packet::IPv4Packet;
@@ -16,13 +17,18 @@ impl Router {
     pub fn send_to_network(&mut self, ipv4_packet: &IPv4Packet) {
         if !ipv4_packet.is_valid() {
             warn!(target: TAG, "Dropping invalid packet");
+            if log_enabled!(target: TAG, LogLevel::Trace) {
+                // TODO log binary
+            }
+        } else {
+            let route = self.get_route(ipv4_packet);
+            // route.send_to_network(ipv4_packet);
         }
-        // TODO
     }
 
-    fn get_route_for(&mut self, ipv4_packet: &IPv4Packet) -> usize {
+    fn get_route(&mut self, ipv4_packet: &IPv4Packet) -> &Route {
         let key = RouteKey::from_packet(ipv4_packet);
-        match self.find_route_index(&key) {
+        let index = match self.find_route_index(&key) {
             Some(index) => index,
             None => {
                 let route = Route::new(&self.client, key, ipv4_packet);
@@ -30,7 +36,8 @@ impl Router {
                 self.routes.push(route);
                 index
             }
-        }
+        };
+        self.routes.get(index).unwrap()
     }
 
     fn find_route_index(&self, key: &RouteKey) -> Option<usize> {
