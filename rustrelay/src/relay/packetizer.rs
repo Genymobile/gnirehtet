9106302b4ce60aa07d13c8d3577465868c3b2224
Romain Vimont <rvimont@genymobile.com>
2017-06-09 21:48:30 +0200

    Store transport_index and payload_index

--- rustrelay/src/relay/packetizer.rs
+++ rustrelay/src/relay/packetizer.rs
@@ -6,6 +6,7 @@ use super::transport_header::TransportHeader;
 /// Convert from level 5 to level 3 by appending correct IP and transport headers.
 pub struct Packetizer {
     buffer: Box<[u8; MAX_PACKET_LENGTH]>,
+    transport_index: usize,
     payload_index: usize,
     ipv4_header: IPv4Header,
     transport_header: TransportHeader,
@@ -18,19 +19,17 @@ impl Packetizer {
         let mut ipv4_header = ipv4_header.clone();
         let mut transport_header = transport_header.clone();
 
-        let headers_length = ipv4_header.header_length as usize +
-                             transport_header.get_header_length() as usize;
-        &mut buffer[..headers_length].copy_from_slice(&raw[..headers_length]);
+        let transport_index = ipv4_header.header_length as usize;
+        let payload_index = transport_index + transport_header.get_header_length() as usize;
+        &mut buffer[..payload_index].copy_from_slice(&raw[..payload_index]);
 
         ipv4_header.switch_source_and_destination(&mut buffer[..]);
-        {
-            let transport_raw = &mut buffer[ipv4_header.header_length as usize..];
-            transport_header.switch_source_and_destination(transport_raw);
-        }
+        transport_header.switch_source_and_destination(&mut buffer[transport_index..]);
 
         Self {
             buffer: buffer,
-            payload_index: headers_length,
+            transport_index: transport_index,
+            payload_index: payload_index,
             ipv4_header: ipv4_header,
             transport_header: transport_header,
         }
@@ -41,16 +40,10 @@ impl Packetizer {
     }
 
     fn inflate(&mut self, payload_length: u16) -> IPv4Packet {
-        let ipv4_header_length = self.ipv4_header.header_length as u16;
-        let transport_header_length = self.transport_header.get_header_length() as u16;
-        let total_length = ipv4_header_length + transport_header_length + payload_length;
+        let total_length = self.payload_index as u16 + payload_length;
 
         self.ipv4_header.set_total_length(&mut self.buffer[..], total_length);
-
-        {
-            let transport_raw = &mut self.buffer[self.ipv4_header.header_length as usize..];
-            self.transport_header.set_payload_length(transport_raw, payload_length);
-        }
+        self.transport_header.set_payload_length(&mut self.buffer[self.transport_index..], payload_length);
 
         let mut ipv4_packet = IPv4Packet::new(&mut self.buffer[..total_length as usize], self.ipv4_header, self.transport_header);
         ipv4_packet.compute_checksums();
