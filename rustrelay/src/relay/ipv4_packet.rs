1103352b24d4e83433abc0251fef4688327fe03a
Romain Vimont <rvimont@genymobile.com>
2017-06-09 15:41:40 +0200

    Factorize TransportHeader::parse()

--- rustrelay/src/relay/ipv4_packet.rs
+++ rustrelay/src/relay/ipv4_packet.rs
@@ -17,11 +17,7 @@ impl<'a> IPv4Packet<'a> {
         let ipv4_header = IPv4Header::parse(raw);
         let transport_header = {
             let payload = &raw[ipv4_header.header_length as usize..];
-            match ipv4_header.protocol {
-                Protocol::UDP => Some(UDPHeader::parse(payload).into()),
-                Protocol::TCP => Some(TCPHeader::parse(payload).into()),
-                _ => None
-            }
+            TransportHeader::parse(ipv4_header.protocol, payload)
         };
         Self {
             raw: raw,
