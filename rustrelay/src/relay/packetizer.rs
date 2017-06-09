880255c218966e75bd39ea5ed76c158541eaf4b1
Romain Vimont <rvimont@genymobile.com>
2017-06-09 17:40:39 +0200

    Pass reference to headers separately
    So that there is no need to have a full IPv4Packet.

--- rustrelay/src/relay/packetizer.rs
+++ rustrelay/src/relay/packetizer.rs
@@ -11,19 +11,19 @@ pub struct Packetizer {
 }
 
 impl Packetizer {
-    pub fn new(ipv4_packet: &IPv4Packet) -> Self {
-        assert!(ipv4_packet.is_valid());
+    pub fn new(raw: &mut [u8], ipv4_header: &IPv4Header, transport_header: &TransportHeader) -> Self {
         let mut buffer = Box::new([0; MAX_PACKET_LENGTH]);
 
-        let mut ipv4_header = ipv4_packet.ipv4_header.clone();
-        let mut transport_header = ipv4_packet.transport_header.as_ref().unwrap().clone();
+        let mut ipv4_header = ipv4_header.clone();
+        let mut transport_header = transport_header.clone();
 
         let headers_length = ipv4_header.header_length as usize +
                              transport_header.get_header_length() as usize;
-        &mut buffer[0..headers_length].copy_from_slice(&ipv4_packet.raw[0..headers_length]);
+        &mut buffer[0..headers_length].copy_from_slice(&raw[0..headers_length]);
 
         ipv4_header.switch_source_and_destination(&mut buffer[..]);
         transport_header.switch_source_and_destination(&mut buffer[ipv4_header.header_length as usize..]);
+
         Self {
             buffer: buffer,
             payload_index: headers_length,
