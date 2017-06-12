abef223d77d967fb0bb43ceadfae3cd1099b01c2
Romain Vimont <rvimont@genymobile.com>
2017-06-12 16:48:37 +0200

    Statically dispatch connections

--- rustrelay/src/relay/connection.rs
+++ rustrelay/src/relay/connection.rs
@@ -1,7 +1,26 @@
 use super::ipv4_packet::IPv4Packet;
+use super::udp_connection::UDPConnection;
 
-pub trait Connection {
-    fn send_to_network(&mut self, ipv4_packet: &IPv4Packet);
-    fn disconnect();
-    fn is_expired() -> bool;
+pub enum Connection {
+    UDP(UDPConnection),
+}
+
+impl Connection {
+    fn send_to_network(&mut self, ipv4_packet: &IPv4Packet) {
+        match *self {
+            Connection::UDP(ref mut udp_connection) => udp_connection.send_to_network(ipv4_packet),
+        }
+    }
+
+    fn disconnect(&mut self) {
+        match *self {
+            Connection::UDP(ref mut udp_connection) => udp_connection.disconnect(),
+        }
+    }
+
+    fn is_expired(&self) -> bool {
+        match *self {
+            Connection::UDP(ref udp_connection) => udp_connection.is_expired(),
+        }
+    }
 }
