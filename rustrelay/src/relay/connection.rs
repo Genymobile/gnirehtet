2aa1ccbaeff8d5b3f7111fc6ee19e154f4fb3ebb
Romain Vimont <rvimont@genymobile.com>
2017-06-13 16:02:32 +0200

    Dynamically dispatch connections
    Make Connection a trait.
    
    Connection was implemented to statically dispatch over TCPConnection and
    UDPConnection.
    
    However, these instance need to be shared between the Route and the
    selector, so they are embedded into an Rc<RefCell<…>>.
    
    Mixing static dispatch with indirection adds complexity and is not
    elegant. Therefore, switch to dynamic dispatch.

--- rustrelay/src/relay/connection.rs
+++ rustrelay/src/relay/connection.rs
@@ -1,32 +1,8 @@
 use super::ipv4_packet::IPv4Packet;
 use super::udp_connection::UDPConnection;
 
-pub enum Connection {
-    UDP(UDPConnection),
-}
-
-impl Connection {
-    pub fn send_to_network(&mut self, ipv4_packet: &IPv4Packet) {
-        match *self {
-            Connection::UDP(ref mut udp_connection) => udp_connection.send_to_network(ipv4_packet),
-        }
-    }
-
-    pub fn disconnect(&mut self) {
-        match *self {
-            Connection::UDP(ref mut udp_connection) => udp_connection.disconnect(),
-        }
-    }
-
-    pub fn is_expired(&self) -> bool {
-        match *self {
-            Connection::UDP(ref udp_connection) => udp_connection.is_expired(),
-        }
-    }
-}
-
-impl From<UDPConnection> for Connection {
-    fn from(udp_connection: UDPConnection) -> Connection {
-        Connection::UDP(udp_connection)
-    }
+pub trait Connection {
+    fn send_to_network(&mut self, ipv4_packet: &IPv4Packet);
+    fn disconnect(&mut self);
+    fn is_expired(&self) -> bool;
 }
--- rustrelay/src/relay/udp_connection.rs
+++ rustrelay/src/relay/udp_connection.rs
@@ -1,3 +1,7 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use super::connection::Connection;
 use super::datagram_buffer::DatagramBuffer;
 use super::ipv4_packet::{IPv4Packet, MAX_PACKET_LENGTH};
 use super::packetizer::Packetizer;
@@ -8,25 +12,27 @@ pub struct UDPConnection {
 }
 
 impl UDPConnection {
-    pub fn new(reference_packet: &IPv4Packet) -> Self {
+    pub fn new(reference_packet: &IPv4Packet) -> Rc<RefCell<Self>> {
         let raw: &[u8] = reference_packet.raw();
         let ipv4_header = reference_packet.ipv4_header().clone();
         let transport_header = reference_packet.transport_header().as_ref().unwrap().clone();
-        Self {
+        Rc::new(RefCell::new(Self {
             client_to_network: DatagramBuffer::new(4 * MAX_PACKET_LENGTH),
             network_to_client: Packetizer::new(raw, ipv4_header, transport_header),
-        }
+        }))
     }
+}
 
-    pub fn send_to_network(&mut self, ipv4_packet: &IPv4Packet) {
+impl Connection for UDPConnection {
+    fn send_to_network(&mut self, ipv4_packet: &IPv4Packet) {
         // TODO
     }
 
-    pub fn disconnect(&mut self) {
+    fn disconnect(&mut self) {
         // TODO
     }
 
-    pub fn is_expired(&self) -> bool {
+    fn is_expired(&self) -> bool {
         // TODO
         false
     }
