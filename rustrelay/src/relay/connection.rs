fd88f64d1b5cc71d3b3418aaf74da8d0db6a25ac
Romain Vimont <rvimont@genymobile.com>
2017-06-13 18:36:31 +0200

    Rewrite 10.0.2.2 to 127.0.0.1

--- rustrelay/src/relay/connection.rs
+++ rustrelay/src/relay/connection.rs
@@ -1,8 +1,22 @@
+use std::net::{Ipv4Addr, SocketAddrV4};
+
 use super::ipv4_packet::IPv4Packet;
-use super::udp_connection::UDPConnection;
+use super::net;
+use super::route::RouteKey;
+
+const LOCALHOST_FORWARD: u32 = 0x0A000202;
 
 pub trait Connection {
     fn send_to_network(&mut self, ipv4_packet: &IPv4Packet);
     fn disconnect(&mut self);
     fn is_expired(&self) -> bool;
+}
+
+pub fn rewritten_destination(ipv4: u32, port: u16) -> SocketAddrV4  {
+    let addr = if ipv4 == LOCALHOST_FORWARD {
+        Ipv4Addr::new(127, 0, 0, 1)
+    } else {
+        net::to_addr(ipv4)
+    };
+    SocketAddrV4::new(addr, port)
 }
