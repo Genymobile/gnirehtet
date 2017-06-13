b30433a685ee78eb72d4594da78bd42fd3fd0106
Romain Vimont <rvimont@genymobile.com>
2017-06-13 17:49:01 +0200

    Pass route key to connections

--- rustrelay/src/relay/udp_connection.rs
+++ rustrelay/src/relay/udp_connection.rs
@@ -6,20 +6,23 @@ use super::connection::Connection;
 use super::datagram_buffer::DatagramBuffer;
 use super::ipv4_packet::{IPv4Packet, MAX_PACKET_LENGTH};
 use super::packetizer::Packetizer;
+use super::route::RouteKey;
 
 pub struct UDPConnection {
     client: Weak<RefCell<Client>>,
+    route_key: RouteKey,
     client_to_network: DatagramBuffer,
     network_to_client: Packetizer,
 }
 
 impl UDPConnection {
-    pub fn new(client: Weak<RefCell<Client>>, reference_packet: &IPv4Packet) -> Rc<RefCell<Self>> {
+    pub fn new(client: Weak<RefCell<Client>>, route_key: RouteKey, reference_packet: &IPv4Packet) -> Rc<RefCell<Self>> {
         let raw: &[u8] = reference_packet.raw();
         let ipv4_header = reference_packet.ipv4_header().clone();
         let transport_header = reference_packet.transport_header().as_ref().unwrap().clone();
         Rc::new(RefCell::new(Self {
             client: client,
+            route_key: route_key,
             client_to_network: DatagramBuffer::new(4 * MAX_PACKET_LENGTH),
             network_to_client: Packetizer::new(raw, ipv4_header, transport_header),
         }))
