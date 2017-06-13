7f64ade8df222c905e44ece39b89c682cd8a6846
Romain Vimont <rvimont@genymobile.com>
2017-06-13 17:47:34 +0200

    Pass weak-client to connections

--- rustrelay/src/relay/udp_connection.rs
+++ rustrelay/src/relay/udp_connection.rs
@@ -1,22 +1,25 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
+use super::client::Client;
 use super::connection::Connection;
 use super::datagram_buffer::DatagramBuffer;
 use super::ipv4_packet::{IPv4Packet, MAX_PACKET_LENGTH};
 use super::packetizer::Packetizer;
 
 pub struct UDPConnection {
+    client: Weak<RefCell<Client>>,
     client_to_network: DatagramBuffer,
     network_to_client: Packetizer,
 }
 
 impl UDPConnection {
-    pub fn new(reference_packet: &IPv4Packet) -> Rc<RefCell<Self>> {
+    pub fn new(client: Weak<RefCell<Client>>, reference_packet: &IPv4Packet) -> Rc<RefCell<Self>> {
         let raw: &[u8] = reference_packet.raw();
         let ipv4_header = reference_packet.ipv4_header().clone();
         let transport_header = reference_packet.transport_header().as_ref().unwrap().clone();
         Rc::new(RefCell::new(Self {
+            client: client,
             client_to_network: DatagramBuffer::new(4 * MAX_PACKET_LENGTH),
             network_to_client: Packetizer::new(raw, ipv4_header, transport_header),
         }))
