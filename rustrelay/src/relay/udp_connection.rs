72fd6ffef19cf807056bad7289fb85d9e5f07997
Romain Vimont <rvimont@genymobile.com>
2017-06-14 14:35:15 +0200

    Implement UDPConnection::new()

--- rustrelay/src/relay/udp_connection.rs
+++ rustrelay/src/relay/udp_connection.rs
@@ -2,6 +2,7 @@ use std::cell::RefCell;
 use std::io;
 use std::rc::{Rc, Weak};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
+use mio::{Event, PollOpt, Ready};
 
 use super::client::Client;
 use super::connection::{self, Connection};
@@ -9,6 +10,7 @@ use super::datagram_buffer::DatagramBuffer;
 use super::ipv4_packet::{IPv4Packet, MAX_PACKET_LENGTH};
 use super::packetizer::Packetizer;
 use super::route::RouteKey;
+use super::selector::Selector;
 
 pub struct UDPConnection {
     client: Weak<RefCell<Client>>,
@@ -31,6 +33,16 @@ impl UDPConnection {
             client_to_network: DatagramBuffer::new(4 * MAX_PACKET_LENGTH),
             network_to_client: Packetizer::new(raw, ipv4_header, transport_header),
         }));
+
+        {
+            let rc_clone = rc.clone();
+            let handler = move |selector: &mut Selector, ready| {
+                let mut self_ref = rc_clone.borrow_mut();
+                self_ref.on_ready(selector, ready);
+            };
+            let mut self_ref = rc.borrow_mut();
+            //let token = selector.register(&self_ref.socket, handler, Ready::readable(), PollOpt::level())?;
+        }
         Ok(rc)
     }
 
@@ -40,6 +52,10 @@ impl UDPConnection {
         let rewritten_destination = connection::rewritten_destination(route_key.destination_ip(), route_key.destination_port());
         udp_socket.connect(rewritten_destination)?;
         Ok(udp_socket)
+    }
+
+    fn on_ready(&mut self, selector: &mut Selector, event: Event) {
+
     }
 }
 
