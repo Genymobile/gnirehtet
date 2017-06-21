cb6be8f1e61e384a282ad1dcd420aad1b98c032c
Romain Vimont <rvimont@genymobile.com>
2017-06-21 17:28:08 +0200

    Add tests for IPv4Packet

--- rustrelay/src/relay/ipv4_packet.rs
+++ rustrelay/src/relay/ipv4_packet.rs
@@ -107,3 +107,93 @@ impl<'a> IPv4Packet<'a> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use byteorder::{BigEndian, ByteOrder, WriteBytesExt};
+    use ::relay::ipv4_header::Protocol;
+
+    fn create_packet() -> Vec<u8> {
+        let mut raw = Vec::new();
+        raw.reserve(32);
+
+        raw.write_u8(4u8 << 4 | 5).unwrap(); // version_and_ihl
+        raw.write_u8(0).unwrap(); //ToS
+        raw.write_u16::<BigEndian>(32).unwrap(); // total length 20 + 8 + 4
+        raw.write_u32::<BigEndian>(0).unwrap(); // id_flags_fragment_offset
+        raw.write_u8(0).unwrap(); // TTL
+        raw.write_u8(17).unwrap(); // protocol (UDP)
+        raw.write_u16::<BigEndian>(0).unwrap(); // checksum
+        raw.write_u32::<BigEndian>(0x12345678).unwrap(); // source address
+        raw.write_u32::<BigEndian>(0x42424242).unwrap(); // destination address
+
+        raw.write_u16::<BigEndian>(1234).unwrap(); // source port
+        raw.write_u16::<BigEndian>(5678).unwrap(); // destination port
+        raw.write_u16::<BigEndian>(4).unwrap(); // length
+        raw.write_u16::<BigEndian>(0).unwrap(); // checksum
+
+        raw.write_u32::<BigEndian>(0x11223344).unwrap(); // payload
+
+        raw
+    }
+
+    #[test]
+    fn parse_headers() {
+        let raw = &mut create_packet()[..];
+        let mut ipv4_packet = IPv4Packet::parse(raw);
+
+        {
+            let ipv4_header = ipv4_packet.ipv4_header();
+            assert_eq!(20, ipv4_header.header_length());
+            assert_eq!(32, ipv4_header.total_length());
+            assert_eq!(Protocol::UDP, ipv4_header.protocol());
+            assert_eq!(0x12345678, ipv4_header.source());
+            assert_eq!(0x42424242, ipv4_header.destination());
+
+            if let Some(TransportHeader::UDP(ref udp_header)) = *ipv4_packet.transport_header() {
+                assert_eq!(1234, udp_header.source_port());
+                assert_eq!(5678, udp_header.destination_port());
+            } else {
+                panic!("No UDP transport header");
+            }
+        }
+
+        ipv4_packet.swap_source_and_destination();
+
+        {
+            let ipv4_header = ipv4_packet.ipv4_header();
+            assert_eq!(0x42424242, ipv4_header.source());
+            assert_eq!(0x12345678, ipv4_header.destination());
+
+            if let Some(TransportHeader::UDP(ref udp_header)) = *ipv4_packet.transport_header() {
+                assert_eq!(5678, udp_header.source_port());
+                assert_eq!(1234, udp_header.destination_port());
+            } else {
+                panic!("No UDP transport header");
+            }
+        }
+
+        {
+            let raw = ipv4_packet.raw();
+            // assert that the buffer has been modified
+            let raw_source = BigEndian::read_u32(&raw[12..16]);
+            let raw_destination = BigEndian::read_u32(&raw[16..20]);
+            assert_eq!(0x42424242, raw_source);
+            assert_eq!(0x12345678, raw_destination);
+
+            if let Some(TransportHeader::UDP(ref udp_header)) = *ipv4_packet.transport_header() {
+                assert_eq!(5678, udp_header.source_port());
+                assert_eq!(1234, udp_header.destination_port());
+            } else {
+                panic!("No UDP transport header");
+            }
+        }
+    }
+
+    #[test]
+    fn payload() {
+        let raw = &mut create_packet()[..];
+        let ipv4_packet = IPv4Packet::parse(raw);
+        assert_eq!([0x11, 0x22, 0x33, 0x44], ipv4_packet.payload());
+    }
+}
