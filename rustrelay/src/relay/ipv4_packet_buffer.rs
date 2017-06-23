d6ca8059be4f085d9cc3ed6e465cf7a96e637283
Romain Vimont <rvimont@genymobile.com>
2017-06-23 22:15:47 +0200

    Fix IPv4PacketBuffer condition

--- rustrelay/src/relay/ipv4_packet_buffer.rs
+++ rustrelay/src/relay/ipv4_packet_buffer.rs
@@ -26,7 +26,7 @@ impl IPv4PacketBuffer {
     fn available_packet_length(&self) -> Option<u16> {
         if let Some((version, length)) = ipv4_header::peek_version_length(&self.buf) {
             assert!(version == 4, "Not an IPv4 packet");
-            if length > self.head as u16 {
+            if length <= self.head as u16 {
                 // full packet available
                 Some(length)
             } else {
