b82a190e4fc0e76af5d0b5d5d2ea8a5dd7ef1ec7
Romain Vimont <rvimont@genymobile.com>
2017-06-23 09:45:45 +0200

    Introduce TCPConnection

--- rustrelay/src/relay/tcp_connection.rs
+++ rustrelay/src/relay/tcp_connection.rs
@@ -1,12 +1,17 @@
 use std::cell::RefCell;
+use std::io;
+use std::net::{Ipv4Addr, SocketAddr};
 use std::rc::{Rc, Weak};
 use mio::{Event, PollOpt, Ready, Token};
 use mio::net::TcpStream;
 
 use super::client::Client;
 use super::connection::{self, Connection, ConnectionId};
+use super::ipv4_packet::{IPv4Packet, MAX_PACKET_LENGTH};
 use super::packetizer::Packetizer;
+use super::selector::Selector;
 use super::stream_buffer::StreamBuffer;
+use super::transport_header::TransportHeader;
 
 const TAG: &'static str = "TCPConnection";
 
@@ -33,4 +38,22 @@ struct TCPConnection {
     their_acknowledgement_numbe: u32,
     client_window: u16,
     remote_closed: bool,
+}
+
+impl TCPConnection {
+/*    pub fn new(selector: &mut Selector, id: ConnectionId, client: Weak<RefCell<Client>>, reference_packet: &IPv4Packet) -> io::Result<Rc<RefCell<Self>>> {
+        let stream = TCPConnection::create_stream(&id)?;
+        let raw = reference_packet.raw();
+        let ipv4_header = reference_packet.ipv4_header().clone();
+        let transport_header = reference_packet.transport_header().as_ref().unwrap().clone();
+        if let TransportHeader::TCP(ref mut tcp_header) = transport_header {
+            tcp_header.shrink_options(raw);
+        }
+        Err()
+    }
+
+    fn create_stream(id: &ConnectionId) -> io::Result<TcpStream> {
+        let rewritten_destination = connection::rewritten_destination(id.destination_ip(), id.destination_port()).into();
+        TcpStream::connect(&rewritten_destination)
+    }*/
 }
