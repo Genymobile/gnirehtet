b1c129b8339304f716d94bf59486bd2e10dd18b1
Romain Vimont <rvimont@genymobile.com>
2017-07-05 14:46:56 +0200

    Rc/RefCell for EventHandlers in Slab
    So that we can get a mut ref to the handler without removing it from the
    Slab.

--- rustrelay/src/relay/selector.rs
+++ rustrelay/src/relay/selector.rs
@@ -25,8 +25,7 @@ impl<T: EventHandler> EventHandler for Rc<RefCell<T>> {
 
 pub struct Selector {
     poll: Poll,
-    handlers: Slab<Box<EventHandler>, Token>,
-    running_state: RunningState,
+    handlers: Slab<Rc<RefCell<Box<EventHandler>>>, Token>,
 }
 
 struct RunningState {
@@ -34,48 +33,18 @@ struct RunningState {
     removed: bool,
 }
 
-impl RunningState {
-    fn new() -> Self {
-        Self {
-            token: None,
-            removed: false,
-        }
-    }
-
-    fn running(&mut self, token: Token) {
-        self.token = Some(token);
-    }
-
-    fn stopping(&mut self) {
-        self.token = None;
-    }
-
-    fn is_running(&self, token: Token) -> bool {
-        self.token.map_or(false, |current_token| current_token == token)
-    }
-
-    fn set_removed(&mut self, removed: bool) {
-        self.removed = removed;
-    }
-
-    fn is_removed(&self) -> bool {
-        self.removed
-    }
-}
-
 impl Selector {
     pub fn new() -> io::Result<Self> {
         Ok(Self {
             poll: Poll::new()?,
             handlers: Slab::with_capacity(1024),
-            running_state: RunningState::new(),
         })
     }
 
     pub fn register<E>(&mut self, handle: &E, handler: Box<EventHandler>,
                    interest: Ready, opts: PollOpt) -> io::Result<Token>
             where E: Evented + ?Sized {
-        let token = self.handlers.insert(handler)
+        let token = self.handlers.insert(Rc::new(RefCell::new(handler)))
                         .map_err(|_| io::Error::new(io::ErrorKind::Other, "Cannot allocate slab slot"))?;
         self.poll.register(handle, token, interest, opts)?;
         Ok(token)
@@ -84,20 +53,12 @@ impl Selector {
     pub fn reregister<E>(&mut self, handle: &E, token: Token,
                    interest: Ready, opts: PollOpt) -> io::Result<()>
             where E: Evented + ?Sized {
-        if self.running_state.is_running(token) {
-            self.running_state.set_removed(false);
-        }
         self.poll.reregister(handle, token, interest, opts)
     }
 
     pub fn deregister<E>(&mut self, handle: &E, token: Token) -> io::Result<()>
             where E: Evented + ?Sized {
-        if self.running_state.is_running(token) {
-            // only mark as removed to not reinsert it after its execution
-            self.running_state.set_removed(true);
-        } else {
-            self.handlers.remove(token).expect("Unknown token removed");
-        }
+        self.handlers.remove(token).expect("Unknown token removed");
         self.poll.deregister(handle)
     }
 
@@ -106,18 +67,7 @@ impl Selector {
     }
 
     pub fn run_handler(&mut self, event: Event) {
-        let mut handler = self.handlers.remove(event.token()).expect("Token not found");
-
-        self.running_state.running(event.token());
-
-        handler.on_ready(self, event);
-
-        if !self.running_state.is_removed() {
-            if let Err(_) = self.handlers.insert(handler) {
-                panic!("Cannot allocate slab slot");
-            }
-        }
-
-        self.running_state.stopping();
+        let mut handler = self.handlers.get_mut(event.token()).expect("Token not found").clone();
+        handler.borrow_mut().on_ready(self, event);
     }
 }
