347e1d5e7a8d17ba889e19b1ed3ef355193820a8
Romain Vimont <rvimont@genymobile.com>
2017-07-12 17:11:45 +0200

    Defer token removal (fix race condition)
    A call to poll() may generate several events, that we handle
    successively.
    
    There was a race condition on deregistration. Suppose that poll()
    generated two events:
     - Token(1) a Client
     - Token(2) a TCPConnection (associated to the client)
    
     During execution of the handler associated to Token(1), the
     TCPConnection is closed, leading to a call to
     selector.deregister(Token(2)).
    
     Then, we want to execute the handler associated to Token(2) (the second
     event), but since it has been deregistered, it does not exist anymore,
     and panic!
    
     To avoid this problem, make Selector.deregister() mark the handlers as
     removed, but kept in selector.handlers until all current event handlers
     are executed.

--- rustrelay/src/relay/selector.rs
+++ rustrelay/src/relay/selector.rs
@@ -25,10 +25,13 @@ impl<T: EventHandler> EventHandler for Rc<RefCell<T>> {
 pub struct Selector {
     poll: Poll,
     handlers: Slab<SelectionHandler, Token>,
+    // tokens to be removed after all the current poll events are executed
+    tokens_to_remove: Vec<Token>,
 }
 
 struct SelectionHandler {
     handler: Rc<RefCell<Box<EventHandler>>>,
+    // registered on Poll, true when !interest.is_empty()
     registered: bool,
 }
 
@@ -46,6 +49,7 @@ impl Selector {
         Ok(Self {
             poll: Poll::new()?,
             handlers: Slab::with_capacity(1024),
+            tokens_to_remove: Vec::new(),
         })
     }
 
@@ -82,11 +86,20 @@ impl Selector {
 
     pub fn deregister<E>(&mut self, handle: &E, token: Token) -> io::Result<()>
             where E: Evented + ?Sized {
-        let selection_handler = self.handlers.remove(token).expect("Unknown token removed");
+        let selection_handler = self.handlers.get_mut(token).expect("Token not found");
         if selection_handler.registered {
             self.poll.deregister(handle)?;
         }
+        // remove them before next poll()
+        self.tokens_to_remove.push(token);
         Ok(())
+    }
+
+    pub fn clean_removed_tokens(&mut self) {
+        for &token in &self.tokens_to_remove {
+            self.handlers.remove(token).expect("Token not found");
+        }
+        self.tokens_to_remove.clear();
     }
 
     pub fn poll(&mut self, events: &mut Events, timeout: Option<Duration>) -> io::Result<usize> {
