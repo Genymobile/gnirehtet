3104563fb915454565ac9c5cf613d7aca5ca5cb0
Romain Vimont <rom@rom1v.com>
2018-09-02 12:53:02 +0200

    Bump version to 2.3

--- relay-rust/src/main.rs
+++ relay-rust/src/main.rs
@@ -34,7 +34,7 @@ use std::thread;
 use std::time::Duration;
 
 const TAG: &'static str = "Main";
-const REQUIRED_APK_VERSION_CODE: &'static str = "5";
+const REQUIRED_APK_VERSION_CODE: &'static str = "6";
 
 const COMMANDS: &[&'static Command] = &[
     &InstallCommand,
