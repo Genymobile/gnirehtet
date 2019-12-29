f8723245b361d4cc9f4cc00e531c8c57167a7f04
Romain Vimont <rom@rom1v.com>
2019-12-29 16:12:36 +0100

    Remove useless return type
    The monitor() function just loops, it cannot fails.

--- relay-rust/src/main.rs
+++ relay-rust/src/main.rs
@@ -448,7 +448,7 @@ fn cmd_autostart(
         let routes = start_routes.as_ref().map(String::as_ref);
         async_start(Some(serial), dns_servers, routes, port)
     }));
-    adb_monitor.monitor()?;
+    adb_monitor.monitor();
     Ok(())
 }
 
